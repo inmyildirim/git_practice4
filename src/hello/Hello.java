8f6130de1b3593005d9761627256b9cf249d083b
Mehmet YILDIRIM <97946779+inmyildirim@users.noreply.github.com>
2022-04-26 17:08:17 +0300

    doing final work on hello feature

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello Word");
-
+        System.out.println("B25 Learning Git and GitHub");
 
 
     }
